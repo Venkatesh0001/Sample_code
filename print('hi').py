907536b9fa36fb56c7253506445254b0f5d1c93e
venkatesh <venkateshmukhopadhyay@gmail.com>
2024-12-07 06:43:21 +0000

    1st code
    Signed-off-by: venkatesh <venkateshmukhopadhyay@gmail.com>

---
+++ print('hi').py
@@ -0,0 +1,38 @@
+# a= int(input('enter the first number'))
+# b= int(input('enter the secound number'))
+
+# print('addtion is :',a+b)
+# print("substraction ius:",a-b)
+
+# a= 'harry'
+
+# print('hi' + a)
+
+# a= int(input('enter the time:'))
+
+# import time
+# TS = time.strftime('%H:%M:%S')
+# print(TS)
+
+
+# TS = time.strftime('%H')
+# print(TS)
+# type(TS)
+
+# a= str(12)
+
+# if (TS < a):
+#     print('good morning')
+# elif(TS==a):
+#     print('good afternoon')
+# else:
+#     print('good evening')
+
+# import time
+# TS = time.strftime('%H:%M:%S')
+# print(TS)
+
+b= 0
+while (b<4):
+    print(b)
+    b= b+1
